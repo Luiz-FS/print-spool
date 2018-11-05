96ea995ccadcc6354623d951c3e9ca3ddbf39895
luiz-fs <luiz.silva@ccc.ufcg.edu.br>
2018-11-05 15:46:14 -0300

    Create lp script

--- check_user_quota.sh
+++ check_user_quota.sh
@@ -2,11 +2,13 @@
 
 user=$1;
 quota=$(cat quota.txt);
-consumed=$(cat table.txt | grep $user | awk '{print $2}'); 
+consumed=$(cat user-status.txt | grep $user | awk '{print $2}'); 
 
 if [ $consumed -gt $quota ] ; then
     echo "Quota exceeded!";
+    exit 1;
 else
     echo "Everything OK!";
+    exit 0;
 fi
 
--- lp.sh
+++ lp.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-echo 'oi'
+file_path=$1;
+username=$USER;
+
+
+function replace_consumed_quota {
+    consumed=$1;
+    sed -i '/'"$username"'/ s/[0-9]\+/'"$consumed"'/g' user-status.txt
+}
+
+function count_pages {
+    page_bytes=$1;
+    echo "a=$page_bytes; b=3600; if ( a%b ) a/b+1 else a/b" | bc
+}
+
+./check_user_quota.sh $username
+
+if [ $? -eq 0 ] ; then
+    file_size=$(ls -l $file_path | awk '{print $5}');
+    user_quota=$(cat user-status.txt | grep "$username" | awk '{print $2}');
+    file_pages=$(count_pages $file_size);
+    consumed=$(expr $user_quota + $file_pages);
+    echo $consumed;
+    replace_consumed_quota $consumed;
+fi
