2904566271759f8e738903632b191d3c2c961f7d
llvieira <lima.lucaslv@gmail.com>
2018-11-07 16:48:56 -0300

    Add log

--- lp.sh
+++ lp.sh
@@ -10,17 +10,28 @@ function replace_consumed_quota {
 }
 
 function count_pages {
-    page_bytes=$1;
-    echo "a=$page_bytes; b=3600; if ( a%b ) a/b+1 else a/b" | bc
+    path=$1;
+    file_size=$(ls -l $path | awk '{print $5}');
+    echo "a=$file_size; b=3600; if ( a%b ) a/b+1 else a/b" | bc;
 }
 
-./check_user_quota.sh $username
+function add_log {
+    job_status=$1;
+    page_number=$2;
+    date_now="$(date +\%F) $(date +\%T)";
+    message="$date_now $username $file_path $page_number $job_status";
+    echo $message >> log.txt;
+}
+
+status=$(./check_user_quota.sh $username);
 
 if [ $? -eq 0 ] ; then
-    file_size=$(ls -l $file_path | awk '{print $5}');
     user_quota=$(cat user-status.txt | grep "$username" | awk '{print $2}');
-    file_pages=$(count_pages $file_size);
+    file_pages=$(count_pages $file_path);
     consumed=$(expr $user_quota + $file_pages);
     echo $consumed;
     replace_consumed_quota $consumed;
+    add_log "Success!" $file_pages;
+else
+    add_log "$status" $(count_pages $file_path);
 fi
