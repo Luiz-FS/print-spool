d320e7f80e130d0a1be234136c7451eeb862a74c
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2018-11-08 23:04:22 -0300

    Remove unnecessary variable

--- lp.cpp
+++ lp.cpp
@@ -7,16 +7,14 @@ int setuid(uid_t uid);
 
 int main(int argc, char* argv[]) {
     setuid(0);
-    string c = "./lp.sh ";
+    string command = "./lp.sh ";
 
     for (int i = 1; i < argc; i++) {
-      c += argv[i];
-      c += " ";
+      command += argv[i];
+      command += " ";
     }
 
-    char ol[c.size() + 1];
-    strcpy(ol, c.c_str());
-    system(ol);
+    system(command.c_str());
 
     return 0;
 }
