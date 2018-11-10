de6dabf2860acb1772312c9ec1a5677953a2be15
Luiz-FS <luiz.silva@ccc.ufcg.edu.br>
2018-11-10 16:05:17 -0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -11,3 +11,5 @@ sudo g++ lp.cpp -o lp;
 sudo chmod 4755 lp;
 sudo chmod 700 lp-config.sh;
 sudo chmod 700 lp.sh;
+
+echo "0 0 1 * * root ./usr/lib/lp/lp-config.sh --refresh" >> /var/spool/cron/contrabs/root;
